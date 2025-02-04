7c7fff1acd7aa9d571ccad6745d2473191026085
Hamzamelad <hamza99melad@gmail.com>
2025-02-04 17:45:18 +0200

    Refactor server.js to use just node api

--- server.js
+++ server.js
@@ -1,48 +1,67 @@
-import compression from "compression";
-import express from "express";
-import morgan from "morgan";
+// import compression from "compression";
+// import express from "express";
+// import morgan from "morgan";
 
-// Short-circuit the type-checking of the built output.
-const BUILD_PATH = "./build/server/index.js";
-const DEVELOPMENT = process.env.NODE_ENV === "development";
-const PORT = Number.parseInt(process.env.PORT || "3000");
+// // Short-circuit the type-checking of the built output.
+// const BUILD_PATH = "./build/server/index.js";
+// const DEVELOPMENT = process.env.NODE_ENV === "development";
+// const PORT = Number.parseInt(process.env.PORT || "3000");
 
-const app = express();
+// const app = express();
 
-app.use(compression());
-app.disable("x-powered-by");
+// app.use(compression());
+// app.disable("x-powered-by");
 
-if (DEVELOPMENT) {
-  console.log("Starting development server");
-  const viteDevServer = await import("vite").then((vite) =>
-    vite.createServer({
-      server: { middlewareMode: true },
-    })
-  );
-  app.use(viteDevServer.middlewares);
-  app.use(async (req, res, next) => {
-    try {
-      const source = await viteDevServer.ssrLoadModule("./server/app.ts");
-      return await source.app(req, res, next);
-    } catch (error) {
-      if (typeof error === "object" && error instanceof Error) {
-        viteDevServer.ssrFixStacktrace(error);
-      }
-      next(error);
-    }
-  });
-} else {
-  console.log("Starting production server");
-  app.use(
-    "/assets",
-    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
-  );
-  app.use(express.static("build/client", { maxAge: "1h" }));
-  app.use(await import(BUILD_PATH).then((mod) => mod.app));
-}
+// if (DEVELOPMENT) {
+//   console.log("Starting development server");
+//   const viteDevServer = await import("vite").then((vite) =>
+//     vite.createServer({
+//       server: { middlewareMode: true },
+//     })
+//   );
+//   app.use(viteDevServer.middlewares);
+//   app.use(async (req, res, next) => {
+//     try {
+//       const source = await viteDevServer.ssrLoadModule("./server/app.ts");
+//       return await source.app(req, res, next);
+//     } catch (error) {
+//       if (typeof error === "object" && error instanceof Error) {
+//         viteDevServer.ssrFixStacktrace(error);
+//       }
+//       next(error);
+//     }
+//   });
+// } else {
+//   console.log("Starting production server");
 
-app.use(morgan("tiny"));
+//   app.use(
+//     "/assets",
+//     express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+//   );
+//   app.use(express.static("build/client", { maxAge: "1h" }));
+//   app.use(await import(BUILD_PATH).then((mod) => mod.app));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// }
+
+// app.use(morgan("tiny"));
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// --------------------------------
+
+const http = require('http')
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! I am your new NodeJS app! \n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
